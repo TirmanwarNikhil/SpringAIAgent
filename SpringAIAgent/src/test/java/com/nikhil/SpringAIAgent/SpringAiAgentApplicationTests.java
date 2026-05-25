f99cf83f04db97c23b04df3b1e49c9a07eef3b5e
Nikhil <techie.nikhilrao@gmail.com>
2026-05-25 10:40:23 +0530

    Initial commit

---
+++ SpringAIAgent/src/test/java/com/nikhil/SpringAIAgent/SpringAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.SpringAIAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
